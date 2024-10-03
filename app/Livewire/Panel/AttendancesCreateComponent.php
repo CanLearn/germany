d791658e5877677fcf7b21eaa5137f4bea564ce9
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-10-04 01:28:07 +0330

    finish developer

---
+++ app/Livewire/Panel/AttendancesCreateComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panel;
+
+use Livewire\Component;
+
+class AttendancesCreateComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.attendances-create-component');
+    }
+}
